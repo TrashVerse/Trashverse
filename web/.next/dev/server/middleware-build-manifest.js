6127d33fbdc3fdf5ab9bbff140a77a347c9f7180
WizTech-create <akobunduwisdom0@gmail.com>
2026-07-14 16:40:41 +0100

    install

--- web/.next/dev/server/middleware-build-manifest.js
+++ web/.next/dev/server/middleware-build-manifest.js
@@ -26,31 +26,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/Trashverse_web_styles_globals_1cd39b69.css",
       "static/chunks/Trashverse_web_pages__app_2da965e7._.js",
       "static/chunks/turbopack-Trashverse_web_pages__app_c85c3f45._.js"
-    ],
-    "/_error": [
-      "static/chunks/a1959_next_dist_compiled_d2cc1b44._.js",
-      "static/chunks/a1959_next_dist_shared_lib_b489f8ac._.js",
-      "static/chunks/a1959_next_dist_client_ff24250f._.js",
-      "static/chunks/a1959_next_dist_345462d0._.js",
-      "static/chunks/a1959_next_error_5347467c.js",
-      "static/chunks/[next]_entry_page-loader_ts_a0af7ff3._.js",
-      "static/chunks/a1959_react-dom_8bc0b7a0._.js",
-      "static/chunks/a1959_050f8cb1._.js",
-      "static/chunks/[root-of-the-server]__e14552b1._.js",
-      "static/chunks/Trashverse_web_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-Trashverse_web_pages__error_45901af2._.js"
-    ],
-    "/blog": [
-      "static/chunks/a1959_next_dist_compiled_930ebb0b._.js",
-      "static/chunks/a1959_next_dist_shared_lib_2315434f._.js",
-      "static/chunks/a1959_next_dist_client_6607d6e3._.js",
-      "static/chunks/a1959_next_dist_91c03e93._.js",
-      "static/chunks/a1959_next_e2586b41._.js",
-      "static/chunks/a1959_react-dom_8bc0b7a0._.js",
-      "static/chunks/a1959_c9204f5f._.js",
-      "static/chunks/[root-of-the-server]__bcfb3f58._.js",
-      "static/chunks/Trashverse_web_pages_blog_2da965e7._.js",
-      "static/chunks/turbopack-Trashverse_web_pages_blog_a06ceeba._.js"
     ]
   },
   "devFiles": [],
